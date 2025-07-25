5bb430886d8fbbfc199063b562e77586903ec662
Dilpreet <155355461+Dilpreet-Sandhu@users.noreply.github.com>
2025-07-25 17:39:46 +0530

    fix: update ESLint and TypeScript configurations to exclude generated files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores : [
+      "node_modules",
+      "/src/generated/"
+    ]
+  }
 ];
 
 export default eslintConfig;
