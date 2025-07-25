306f48dc385bb8f6848f448404d4ebd3e7526e5d
Dilpreet <155355461+Dilpreet-Sandhu@users.noreply.github.com>
2025-07-25 16:26:17 +0530

    fix: await props for searchParams and params in EventQuestionsPage

--- src/app/(auth)/events/[ownerId]/[eventSlug]/(questions)/page.tsx
+++ src/app/(auth)/events/[ownerId]/[eventSlug]/(questions)/page.tsx
@@ -21,10 +21,10 @@ type SearchParams = {
 }
 
 const EventQuestionsPage = async (
-  props: {params : PathParams,searchParams ?:{[key: string]: string | string[] | undefined}}
+  props: {params : Promise<PathParams>,searchParams ?:Promise<{[key: string]: string | string[] | undefined}>}
 ) => {
-  const searchParams =  props.searchParams;
-  const params =  props.params;
+  const searchParams =  await props.searchParams;
+  const params =  await props.params;
 
   const {ownerId,eventSlug} =  params;
   const sortBy = ( searchParams)?.sortBy ?? "newest";
